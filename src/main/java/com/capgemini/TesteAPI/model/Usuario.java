ce9d14440d60ecf4b356822499c85c4adab540c5
fesoares <felipeaugustodasilvasoares@gmail.com>
2022-08-08 16:12:45 -0300

    Primeiro commit, de uma API Rest feita com Spring Boot.

---
+++ src/main/java/com/capgemini/TesteAPI/model/Usuario.java
@@ -0,0 +1,55 @@
+package com.capgemini.TesteAPI.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+@Table(name = "SLDG_TDIVISA")
+@AttributeOverride(name = "id", column = @Column(name = "OID_DIVISA", unique = true, nullable = false, length = 36))
+public class Usuario implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long id;
+    @Column(name = "COD_ISO")
+    private String code;
+    @Column(name = "DES_DIVISA")
+    private String description;
+    @Column(name = "DES_SIMBOLO")
+    private String symbol;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+}
